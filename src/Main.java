9c95a41d12d153316a1f83fa6a465b01b4a9cded
Stefi <sp928@cam.ac.uk>
2020-03-12 18:36:05 +0000

    input

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 public class Main {
   public static void main(String[] args) throws FileNotFoundException {
     InputReader ir = new InputReader();
-    ir.read();
+    Input input = ir.read();
+    input.print();
   }
 }
